3a04ec68e6e4b53fe693109e20d932ce56f1cca3
Geet <geet.exe@gmail.com>
2017-04-21 00:33:39 +0530

    deletion of old code from weather factory

--- js/factories.js
+++ js/factories.js
@@ -19,15 +19,6 @@ app.factory("weatherAPI", function($http, $rootScope){
   return {
     getData: function(){
       return $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$rootScope.city+"&appid=f4ef0931edd7df0498dd3b62d27c0ff6&units=metric")
-      /*.then(function(response){
-				$rootScope.des = response.data.weather[0].description.toUpperCase();
-				var appendWeather = '<span class="white">'+ "Temperature in "+response.data.name+": " +'</span><br>'+' '+response.data.main.temp+'&deg;C <br>'+$rootScope.des+'<br>';
-				$("#appendhere").append(appendWeather);
-			}, function(error){
-				var err = error.status===-1 ? "CORS issue." : "Error.";
-				var appendWeather = '<span class="white">Error!:</span><br>'+' '+err+'<br>';
-				$("#appendhere").append(appendWeather);
-			});  */
     }  
   };
 });
